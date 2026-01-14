3e7f6403e2c446d78a5deb5420e604bf8fa46b0e
Heytor Cardoso (SOUZA) <heytorcardosomachado@gmail.com>
2026-01-14 13:56:23 -0300

    Desenvolvimento inicial do botão de ···

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
+const test = document.getElementById("selector");
+const count = test.children.length;
+console.log(count);
+
 function selectButton(btn) {
     document.querySelectorAll(".options").forEach(b => b.classList.remove("selected"));
     btn.classList.add("selected");
+    const test = document.getElementById("selector");
+    const count = test.childElementCount;
+    console.log(count);
 }
